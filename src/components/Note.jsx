322e74efaaf92a9f6e4b35b5f1b54a1023b3fc7b
Chloe <nguyeney02@gmail.com>
2024-05-25 13:44:47 +0300

    WIP

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+export default function Note({ note, toggleImportance }) {
+  const label = note.important ? "make not important" : "make important";
+
+  return (
+    <li className="note">
+      <span>{note.content}</span>
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  );
+}
